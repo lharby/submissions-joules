08e549926d317356d97900758402393cbf69abe2
Luke Harby <luke.harby@contractor.isbanuk.com>
2020-08-07 10:53:09 +0100

    Tidy up linting

--- src/Includes/Footer/Footer.jsx
+++ src/Includes/Footer/Footer.jsx
@@ -5,12 +5,12 @@ import FooterNavigation from '../../Pages/FooterNavigation/FooterNavigation.jsx'
 import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <footer className="pos-fix xs-font full-width">
-      <FooterNavigation />
-      <span className="pos-abs copyright">&copy; <span className="year">{getCurrentYear()}</span> Luke Harby</span>
-    </footer>
-  );
+    return (
+        <footer className="pos-fix xs-font full-width">
+            <FooterNavigation />
+            <span className="pos-abs copyright">&copy; <span className="year">{getCurrentYear()}</span> Luke Harby</span>
+        </footer>
+    );
 };
 
 export default Footer;
